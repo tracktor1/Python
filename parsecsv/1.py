690696a5b582778330a0191e211033d568b786b1
tracktor1 <39525540+tracktor1@users.noreply.github.com>
2018-11-18 14:55:05 +0200

    Create 1.py

---
+++ parsecsv/1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import csv
+from func import *
+
+
+print "Current path: " ,os.getcwd()
+print "Relative path: " ,relative_path("data.csv") ,"\n"
+
+
+
+with open(relative_path("data.csv")) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_reader) # Skip first line
+    for row in csv_reader:
+            #print row
+            #print(", ".join(row))
+        company = row[0]
+        user = row[1]
+        upass = row[2]
+        ip = validate_ip(row[3])
+        
+        print company, user, upass, ip
+        
