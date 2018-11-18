3318f772055765643d95af6e95f154c9db7133ab
tracktor1 <39525540+tracktor1@users.noreply.github.com>
2018-11-18 17:33:51 +0200

    Update 1.py

--- parsecsv/1.py
+++ parsecsv/1.py
@@ -4,8 +4,8 @@ import csv
 from func import *
 
 
-print "Current path: " ,os.getcwd()
-print "Relative path: " ,relative_path("data.csv") ,"\n"
+print("Current path: " ,os.getcwd())
+print("Relative path: " ,relative_path("data.csv") ,"\n")
 
 
 
@@ -13,12 +13,12 @@ with open(relative_path("data.csv")) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     next(csv_reader) # Skip first line
     for row in csv_reader:
-            #print row
-            #print(", ".join(row))
+          #print(row)
+          #print(", ".join(row))
         company = row[0]
         user = row[1]
         upass = row[2]
         ip = validate_ip(row[3])
         
-        print company, user, upass, ip
+        print(company, user, upass, ip)
         
