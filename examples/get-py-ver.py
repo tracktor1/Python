9d7fd3f4b4591cd49cb6d4243b552b12075d0430
tracktor1 <39525540+tracktor1@users.noreply.github.com>
2018-11-19 16:10:32 +0200

    Create get-py-ver.py

---
+++ examples/get-py-ver.py
@@ -0,0 +1,12 @@
+from sys import exit
+from platform import python_version
+
+
+pver = python_version()
+pv = int(pver[0])
+#if python version is not 2.7 EXIT!
+if pv >= 3:
+   print("Sorry, requires Python 2.x, not Python 3.x")
+   exit(1)
+else:
+   print('Python version is: ', python_version())
