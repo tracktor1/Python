01829392cfb6801da118fb5b86699e3679802ed6
David <39525540+tracktor1@users.noreply.github.com>
2018-11-25 23:29:42 +0200

    Update 1.py

--- parsecsv/1.py
+++ parsecsv/1.py
@@ -15,12 +15,15 @@ with open(relative_path("data.csv")) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     next(csv_reader) # Skip first line
     for row in csv_reader:
-          #print(row)
-          #print(", ".join(row))
         company = row[0]
         user = row[1]
         upass = row[2]
         ip = validate_ip(row[3])
-        
         print(company, user, upass, ip)
+        if ip != "Invalid IP":
+            print("run command with date from vars", ip)
+        else:
+            print("Please fix the ip in this line")
+            
+        
         
